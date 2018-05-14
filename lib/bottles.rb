e1023f5968fda41c4fedaab865e61935c4a7757b
joshuajmurray <joshuajmurray@gmail.com>
2018-05-14 16:56:33 -0700

    Refactored if else in bottles class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,11 +2,7 @@ class Bottles
 
   def verse(count)
     "#{count} bottles of beer on the wall, #{count} bottles of beer.\n" +
-    if count > 2
-      "Take one down and pass it around, #{count - 1} bottles of beer on the wall.\n"
-    else
-      "Take one down and pass it around, #{count - 1} bottle of beer on the wall.\n"
-    end
+    "Take one down and pass it around, #{count - 1} #{ count > 2 ? "bottles" : "bottle" } of beer on the wall.\n"
   end
 
 end
