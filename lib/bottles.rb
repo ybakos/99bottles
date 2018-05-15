d7a3239a4f8e0614d5572652676e75bd4fbdc745
joshuajmurray <joshuajmurray@gmail.com>
2018-05-14 17:17:50 -0700

    Pass count 0 test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,20 @@
 class Bottles
 
   def verse(count)
-    "#{count} bottles of beer on the wall, #{count} bottles of beer.\n" +
-    "Take one down and pass it around, #{count - 1} #{ count > 2 ? "bottles" : "bottle" } of beer on the wall.\n"
+    if count == 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
+      bottle = count == 1 ? "bottle" : "bottles"
+      "#{count} #{bottle} of beer on the wall, " +
+      "#{count} #{bottle} of beer.\n" +
+      if count == 1
+        "Take it down and pass it around, no more bottles of beer on the wall.\n"
+      else
+        "Take #{ count == 1 ? "it" : "one" } down and pass it around, #{count - 1} " +
+        "#{ count > 2 ? "bottles" : "bottle" } of beer on the wall.\n"
+      end
+    end
   end
 
 end
