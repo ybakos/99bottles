fa3759c7e67529b19ab461fbdd467e3e82f80a68
Leo G <leo.gothberg@gmail.com>
2018-05-16 15:59:20 -0700

    Refactor verse method to use switch case instead of if statements.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,19 +1,16 @@
 class Bottles
 
-  def verse (num_bottles)
-    output = "#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.\n"
-
-    if num_bottles == 0
-      output = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    elsif num_bottles == 1
-      output = "#{num_bottles} bottle of beer on the wall, #{num_bottles} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    elsif num_bottles - 1 > 1
-      output += "Take one down and pass it around, #{num_bottles - 1} bottles of beer on the wall.\n"
+  def verse(num_bottles)
+    case num_bottles
+    when 2
+      "#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.\nTake one down and pass it around, #{num_bottles - 1} bottle of beer on the wall.\n"
+    when 1
+      "#{num_bottles} bottle of beer on the wall, #{num_bottles} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
-      output += "Take one down and pass it around, #{num_bottles - 1} bottle of beer on the wall.\n"
+      "#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.\nTake one down and pass it around, #{num_bottles - 1} bottles of beer on the wall.\n"
     end
-
-    output
   end
 
   def verses(first, last)
