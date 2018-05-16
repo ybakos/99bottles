a4383421b49704a64237ad189b63109005361b48
Curtis Baney <curtistheiii@gmail.com>
2018-05-16 12:04:41 -0700

    Add verse method to Bottles class

---
+++ lib/bottles.rb
@@ -0,0 +1,23 @@
+class Bottles
+
+  def verse(verse_number)
+
+    if verse_number > 2
+      return"#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n" +
+      "Take one down and pass it around, #{verse_number-1} bottles of beer on the wall.\n"
+    elsif verse_number == 2
+      return"#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n" +
+      "Take one down and pass it around, #{verse_number-1} bottle of beer on the wall.\n"
+    elsif verse_number == 1
+      return"#{verse_number} bottle of beer on the wall, #{verse_number} bottle of beer.\n" +
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    elsif verse_number == 0
+      return "No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    end
+
+  end
+
+  
+
+end
