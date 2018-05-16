3f494cf0d6339d8e50631f80fb6271d48aa3c047
Leo G <leo.gothberg@gmail.com>
2018-05-15 22:32:52 -0700

    Update argument for verse method, use interpolated strings to dynamically update output.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,7 @@
 class Bottles
 
-  def verse (_)
-    "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+  def verse (num_bottles)
+    "#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.\nTake one down and pass it around, #{num_bottles - 1} bottles of beer on the wall.\n"
   end
 
 end
