32cc86d3f7c11aeb6cf6f8d933d031d7b6c52147
Leo G <leo.gothberg@gmail.com>
2018-05-15 22:45:53 -0700

    Add if statement for edge case of last verse.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,12 +1,14 @@
 class Bottles
 
   def verse (num_bottles)
-    output = "#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.\nTake one down and pass it around, "
+    output = "#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.\n"
 
-    if num_bottles - 1 > 1
-      output += "#{num_bottles - 1} bottles of beer on the wall.\n"
+    if num_bottles == 1
+      output = "#{num_bottles} bottle of beer on the wall, #{num_bottles} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    elsif num_bottles - 1 > 1
+      output += "Take one down and pass it around, #{num_bottles - 1} bottles of beer on the wall.\n"
     else
-      output += "#{num_bottles - 1} bottle of beer on the wall.\n"
+      output += "Take one down and pass it around, #{num_bottles - 1} bottle of beer on the wall.\n"
     end
 
     output
