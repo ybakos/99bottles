a9410bc0906bb060edf015950816dfbbbfbb0235
Leo G <leo.gothberg@gmail.com>
2018-05-15 22:40:15 -0700

    Add if statement to verse method to account for grammar.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,16 @@
 class Bottles
 
   def verse (num_bottles)
-    "#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.\nTake one down and pass it around, #{num_bottles - 1} bottles of beer on the wall.\n"
+    output = "#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.\nTake one down and pass it around, "
+
+    if num_bottles - 1 > 1
+      output += "#{num_bottles - 1} bottles of beer on the wall.\n"
+    else
+      output += "#{num_bottles - 1} bottle of beer on the wall.\n"
+    end
+
+    output
+
   end
 
 end
